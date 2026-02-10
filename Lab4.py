baa9c895eb2b59a693ed123c9365035768ff5fb1
deathbuycactus <131403532+deathbuycactus@users.noreply.github.com>
2026-02-10 23:38:14 +0000

    finish load_pdfs_to_collection function

--- Lab4.py
+++ Lab4.py
@@ -39,9 +39,20 @@ def add_to_collection(collection, text, file_name):
     )
 
 def extract_text_from_pdf(pdf_path):
-    
-def load_pdfs_to_colletion(folder_path, collection):
+    reader = PdfReader(pdf_path)
+    text = ""
 
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text != None:
+            text += page_text
+
+    return text
+
+def load_pdfs_to_colletion(folder_path, collection):
+    
+    return
+# Check if collection is empty and load PDFs
 if collection.count() == 0:
     loaded = load_pdfs_to_colletion('./Lab-04-Data/', collection)
 

7ceaf204da0a1276bb6f37f854a019884b9a9faa
deathbuycactus <131403532+deathbuycactus@users.noreply.github.com>
2026-02-10 20:11:02 +0000

    lab 4 default

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ pg = st.navigation({
     "Labs": [
         st.Page("Lab1.py", title="Lab 1"),
         st.Page("Lab2.py", title="Lab 2"),
-        st.Page("Lab3.py", title="Lab 3", default=True),
-        st.Page("Lab4.py", title="Lab 4"),
+        st.Page("Lab3.py", title="Lab 3"),
+        st.Page("Lab4.py", title="Lab 4", default=True),
         st.Page("Lab5.py", title="Lab 5"),
         st.Page("Lab6.py", title="Lab 6"),
         st.Page("Lab7.py", title="Lab 7"),
